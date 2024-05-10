4dbbe76b488f54f3bf1f4403e0da665e3ecaca48
astroboi_m2 <terry007x@onycom.com>
2024-05-10 18:02:26 +0900

    add class TemplateMatcher

--- diff_spotter.py
+++ diff_spotter.py
@@ -1,6 +1,10 @@
 import cv2
 from PIL import Image, ImageChops
 import numpy as np
+import math
+import scipy.ndimage
+import imagecodecs
+import imreg
 
 
 class DiffSpotter:
@@ -97,3 +101,119 @@ class Utils:
                                          flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
 
         return aligned_im2
+
+    def rgb2gray(self, rgb, scale=None):
+        """Return float grayscale image from RGB24 or RGB48 image."""
+        scale = np.iinfo(rgb.dtype).max if scale is None else scale
+        scale = np.array([[[0.299, 0.587, 0.114]]], np.float32) / scale
+        return np.sum(rgb * scale, axis=-1)
+
+    def rgba2rgb(self, rgba, background=(255, 255, 255)):
+        row, col, ch = rgba.shape
+
+        if ch == 3:
+            return rgba
+
+        assert ch == 4, 'RGBA image has 4 channels.'
+
+        rgb = np.zeros((row, col, 3), dtype='float32')
+        r, g, b, a = rgba[:, :, 0], rgba[:, :, 1], rgba[:, :, 2], rgba[:, :, 3]
+
+        a = np.asarray(a, dtype='float32') / 255.0
+
+        R, G, B = background
+
+        rgb[:, :, 0] = r * a + (1.0 - a) * R
+        rgb[:, :, 1] = g * a + (1.0 - a) * G
+        rgb[:, :, 2] = b * a + (1.0 - a) * B
+
+        return np.asarray(rgb, dtype='uint8')
+
+
+class TemplateMatcher:
+    def __init__(self):
+        pass
+
+    def brute_force_scale_invariant_template_matching(
+            self,
+            template,  # grayscale image
+            search,  # scaled and cropped grayscale image
+            zooms=(1.0, 0.5, 0.25),  # sequence of zoom factors to try
+            size=None,  # power-of-two size of square sliding window
+            delta=None,  # advance of sliding windows. default: half window size
+            min_overlap=0.25,  # minimum overlap of search with window
+            max_diff=0.05,  # max average of search - window differences in overlap
+            max_angle=0.5,  # no rotation
+    ):
+        """Return yoffset, xoffset, and scale of first match of search in template.
+
+        Iterate over scaled versions of the template image in overlapping sliding
+        windows and run FFT-based algorithm for translation, rotation and
+        scale-invariant image registration until a match of the search image is
+        found in the sliding window.
+
+        """
+        if size is None:
+            size = int(pow(2, int(math.log(min(search.shape), 2))))
+        if delta is None:
+            delta = size // 2
+        search = search[:size, :size]
+        for zoom in zooms:
+            windows = np.lib.stride_tricks.sliding_window_view(
+                scipy.ndimage.zoom(template, zoom), search.shape
+            )[::delta, ::delta]
+            print(windows.shape)
+            for i in range(windows.shape[0]):
+                for j in range(windows.shape[1]):
+                    print(f'{zoom}>>', end='')
+                    window = windows[i, j]
+                    im2, scale, angle, (t0, t1) = imreg.similarity(window, search)
+                    diff = np.abs(im2 - window)[im2 != 0]
+                    if (
+                            abs(angle) < max_angle
+                            and diff.size / window.size > min_overlap
+                            and np.mean(diff) < max_diff
+                    ):
+                        return (
+                            (i * delta - t0) / zoom,
+                            (j * delta - t1) / zoom,
+                            1 / scale / zoom,
+                        )
+        raise ValueError('no match of search image found in template')
+
+    def template_matching(self, im1_path, im2_path):
+        utils = Utils()
+        template = imagecodecs.imread(im1_path)
+        search = imagecodecs.imread(im2_path)
+
+        if template.shape[0] * template.shape[1] > search.shape[0] * search.shape[1]:
+            tmp = template.copy()
+            template = search
+            search = tmp
+
+        print(f"{(search.shape[0] * search.shape[1]) / (template.shape[0] * template.shape[1])}")
+
+        multi_n = math.ceil((search.shape[0] * search.shape[1]) / (template.shape[0] * template.shape[1]) * 10) - 10
+        print(multi_n)
+        template = utils.rgba2rgb(template)
+        search = utils.rgba2rgb(search)
+
+        zooms = [1 + zoom / 10 for zoom in range(multi_n + 1)]
+        print(f"init zooms {zooms}")
+        trial_n = 0
+        while True:
+            if trial_n == 100: break
+            try:
+                yoffset, xoffset, scale = self.brute_force_scale_invariant_template_matching(
+                    utils.rgb2gray(template), utils.rgb2gray(search), zooms=zooms
+                )
+                return 1 / scale
+            except Exception as err:
+                print(f"[ERROR] {err}")
+                zooms = [i * 1.01 if i != zooms[-1] else i * 0.999 for i in zooms]
+                if zooms[-2] > zooms[-1]:
+                    print("Reduce ratio")
+                    break
+                print(zooms)
+                trial_n += 1
+                pass
